dadf2406aa27764daf79fcb899e8149bcf1d248e
Joseph Keen <icrona@gmail.com>
2023-03-24 14:37:17 -0600

    Add support for height

--- contextMenu.js
+++ contextMenu.js
@@ -1,10 +1,10 @@
-import OBR from "@owlbear-rodeo/sdk";
+import OBR, { buildImage } from "@owlbear-rodeo/sdk";
 
 const ID = "com.show-distances.menu";
 
 export function setupContextMenu() {
   OBR.contextMenu.create({
-    id: `${ID}/context-menu`,
+    id: `${ID}/context-menu.distance`,
     icons: [
       {
         icon: "/distances.svg",
@@ -31,6 +31,69 @@ export function setupContextMenu() {
           vertical: "TOP",
         },
       });
+    },
+  });
+  OBR.contextMenu.create({
+    id: `${ID}/context-menu.height`,
+    icons: [
+      {
+        icon: "/wing.svg",
+        label: "Set Height",
+        filter: {
+          every: [{ key: "layer", value: "CHARACTER" }],
+        },
+      },
+    ],
+    async onClick(context) {
+      const dpi = await OBR.scene.grid.getDpi()
+      const scale = await OBR.scene.grid.getScale()
+      const item_height = parseInt(window.prompt(`Enter the vertical height of this combatant in ${scale.parsed.unit}`));
+      let objects = []
+      OBR.scene.items.updateItems(context.items, (items) => {
+        for (let item of items) {
+          if (item_height && !isNaN(item_height)) {
+            const dpiScale = dpi / item.grid.dpi;
+            const height = item.image.height * dpiScale * item.scale.y;
+            const offsetY = (item.grid.offset.y / item.image.height) * height;
+            // Apply image offset and offset position to be centered just above the token label
+            const position = {
+              x: item.position.x,
+              y: item.position.y + offsetY - 50,
+            };
+
+            const image = buildImage({
+                height: 40,
+                width: 40,
+                url: "http://localhost:5173/wing.png",
+                mime: "image/png",
+              },
+              { 
+                dpi: 40, 
+                offset: { x: 20, y: 20 } 
+              })
+            .position(position)
+            .layer("ATTACHMENT")
+            .scale({x: 0.3, y: .3})
+            .plainText(`${item_height} ${scale.parsed.unit}.`)
+            .locked(true)
+            .attachedTo(item.id)
+            .id(`${ID}/context-menu.height/${item.id}`)
+            .build()
+            objects.push(image)
+            item.metadata[`${ID}/metadata`] = {
+              item_height,
+            };
+          } else if (item_height == 0) {
+            const ids = context.items.map((item) => `${ID}/context-menu.height/${item.id}`);
+            OBR.scene.items.deleteItems(ids);
+            item.metadata[`${ID}/metadata`] = {
+              item_height,
+            };
+          }
+        }
+      });
+      OBR.scene.items.addItems(objects);
+      
     },
   });
 }
--- show-distances.js
+++ show-distances.js
@@ -1,32 +1,41 @@
 import OBR, {isImage} from "@owlbear-rodeo/sdk";
 import "./style.css";
 
-function calcDistance(coord1, coord2, measurement, scale) {
+function calcDistance(coord1, coord2, measurement, scale, height_difference) {
     const multiplier = scale.parsed.multiplier
     const digits = scale.parsed.digits
 
     let distance = Infinity
     const deltaX = Math.abs(coord1.x - coord2.x);
     const deltaY = Math.abs(coord1.y - coord2.y);
+    const deltaZ = Math.abs(height_difference) / multiplier
 
     switch (measurement) {
         case 'CHEBYSHEV':
-            distance = Math.max(deltaX, deltaY)
+            distance = Math.max(deltaX, deltaY, deltaY)
             distance *= multiplier
             break
         case "ALTERNATING":
-            let n_diag = Math.min(deltaX, deltaY)
-            let n_straight = Math.max(deltaX, deltaY) - n_diag
-            distance = Math.round(multiplier * n_straight + ((3*multiplier) * Math.floor(n_diag / 2)) + (multiplier * (n_diag % 2)))
+            let n_diag = Math.min(deltaX, deltaY);
+            let n_straight = Math.max(deltaX, deltaY) - n_diag;
+            distance = Math.round(
+                multiplier * n_straight +
+                (3 * multiplier) * Math.floor(n_diag / 2) +
+                (multiplier * (n_diag % 2)) +
+                (multiplier * deltaZ)
+            );
             break
         case "EUCLIDEAN":
             distance = Math.sqrt(
                 Math.pow(coord1.x - coord2.x, 2) + Math.pow(coord1.y - coord2.y, 2)
             );
+            distance = Math.sqrt(
+                Math.pow(distance, 2) + Math.pow(deltaZ, 2)
+            )
             distance *= multiplier
             break
         case "MANHATTAN":
-            distance = deltaX + deltaY
+            distance = deltaX + deltaY + deltaY
             distance *= multiplier
             break
     }    
@@ -47,6 +56,11 @@ export async function getDistances(target) {
         (item) => item.layer === "CHARACTER" && isImage(item) && (is_dm || item.visible)
     );
 
+
+    let item_height = 0            
+    if (target.metadata[`com.show-distances.menu/metadata`] != undefined) {
+        item_height = target.metadata[`com.show-distances.menu/metadata`].item_height
+    }
     
     let item_scale = Math.max(1, Math.floor(target.scale.x))
     let item_bottom_right = {
@@ -69,7 +83,13 @@ export async function getDistances(target) {
             } else {
                 name = `<strong>${character.name}</strong>`
             }
-            
+
+            let character_height = 0            
+            if (character.metadata[`com.show-distances.menu/metadata`] != undefined) {
+                character_height = character.metadata[`com.show-distances.menu/metadata`].item_height
+            }
+            const height_difference = character_height - item_height
+
             if (!character.visible) {
                 name = `<span class='invisible'>${name}</span>`
             }
@@ -89,7 +109,7 @@ export async function getDistances(target) {
                             const square2X = character_bottom_right.x - k;
                             const square2Y = character_bottom_right.y - l;
             
-                            let distance = calcDistance({x: square1X, y: square1Y}, {x: square2X, y: square2Y}, measurement, scale)
+                            let distance = calcDistance({x: square1X, y: square1Y}, {x: square2X, y: square2Y}, measurement, scale, height_difference)
                             
                             if (distance < closestDistance) {
                                 closestDistance = distance;
@@ -101,18 +121,18 @@ export async function getDistances(target) {
 
             distances.push({
                 target: name,
-                distance: closestDistance
+                distance: closestDistance,
+                height: height_difference
             })
         }
     });
     distances.sort((a, b) => {
         return a.distance - b.distance
     }).forEach(dist => {
-        table += `<tr><td>${dist.target}</td><td>${dist.distance} ${scale.parsed.unit}. away</td></tr>`
+        table += `<tr><td>${dist.target}</td><td>${dist.distance} ${scale.parsed.unit}. away ${dist.height ? (dist.height > 0 ? '↑' : '↓') : ''}</td></tr>`
     })
     return table
 }
-
 OBR.onReady(async () => {
     const selection = await OBR.player.getSelection()
     if (selection) {
