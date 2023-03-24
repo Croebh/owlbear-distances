26ff4e8dd2c579386144649e4835f71e9f31464c
Joseph Keen <icrona@gmail.com>
2023-03-24 00:43:55 -0600

    Update height as you go

--- mainList.js
+++ mainList.js
@@ -9,14 +9,13 @@ export async function setupList(element) {
     const characters = await OBR.scene.items.getItems(
         (item) => item.layer === "CHARACTER" && isImage(item) && (is_dm || item.visible)
     );
-    
+    let last_selection = [];
     const renderList = async (player) => {
         const nodes = [];
         if (player.selection) {
             const items = await OBR.scene.items.getItems(player.selection);
             for (const item of items) {
                 if (item.layer != "CHARACTER") {
-                    console.log(item.layer);
                     continue
                 }
                 let name = item.text.plainText.replace(/(\r\n|\n|\r)/gm, "");
@@ -28,13 +27,21 @@ export async function setupList(element) {
                 const node = document.createElement("h3");
                 node.innerHTML = `${name}`;
                 nodes.push(node)
-
+    
                 const table = document.createElement("table");
                 let distances = await getDistances(item)
                 table.innerHTML = distances
                 nodes.push(table)
             }
             element.replaceChildren(...nodes);
+            OBR.action.setHeight(Math.min(document.querySelector("#app").offsetHeight + 20, 600));
+        } else {
+            document.querySelector("#app").innerHTML = `<h3 style='text-align: center'>No character selected</h3>`;
+            OBR.action.setHeight(40);
+        }
+        if (!document.querySelector("#app").innerHTML) {
+            document.querySelector("#app").innerHTML = `<h3 style='text-align: center'>No character selected</h3>`;
+            OBR.action.setHeight(40);
         }
     }    
     OBR.player.onChange(renderList);
